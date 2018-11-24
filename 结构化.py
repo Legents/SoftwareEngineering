1674ad7ac988986e4f02fd83482d5baf6135b056
Legents <li_guannan@163.com>
2018-11-24 23:43:59 +0800

    计算完成，合并完成

--- 结构化.py
+++ 结构化.py
@@ -1,127 +1,98 @@
 import csv
 import math
 import pandas as pd
-
+#数据处理
 def data_process(file):
-	Source,Target,Weight = [],[],[]
+	Source,Target,Parameter = [],[],[]
 	data = []
-	with open(file) as f1:
-		reader = csv.reader(f1)
+	with open(file) as f:
+		reader = csv.reader(f)
 		header_row = next(reader)
 		for row in reader:
-			try:
-				source = int(row[0])
-				target = int(row[1])
-				weight = int(row[2])			
-			except ValueError:
-				print(current_date,'missing data')
-			else:
-				Source.append(source)
-				Target.append(target)
-				Weight.append(weight)
-	print(Source)
-	data.append(Source)
-	print(Target)
-	data.append(Target)
-	print(Weight)
-	data.append(Weight)
-	return data
-	
-def Sigmoid(data):
-	sigmoid = []
-	for x in data[2]:
-		s = 1.0/(1 + math.exp(-x*1.0))
-		sigmoid.append(s)
-	#print(sigmoid)
-	data.append(sigmoid)
-	return data
-
-def write_sig(file,data):
-	dataframe = pd.DataFrame({'Source':data[0],'Target':data[1],'Weight':data[2],'Sigmoid':data[3]})
-	dataframe.to_csv(file,index=False,sep=',')
-
-def data_process_2(file):
-	Source,Target,Weight,Sigmoid = [],[],[],[]
-	data = []
-	with open(file) as f1:
-		reader = csv.reader(f1)
-		header_row = next(reader)
-		for row in reader:
-			source = int(row[0])
-			target = int(row[1])
-			weight = int(row[2])
-			sigmoid = row[3]
-						
+			source = row[0]
+			target = row[1]
+			parameter = row[2]
 			Source.append(source)
 			Target.append(target)
-			Weight.append(weight)
-			Sigmoid.append(sigmoid)
+			Parameter.append(parameter)
 	data.append(Source)
 	data.append(Target)
-	data.append(Weight)
-	data.append(Sigmoid)
+	data.append(Parameter)
 	return data
-	
+#sigmoid归一化处理	
+def Sigmoid(data):
+	sigmoid = []
+	for a in data[2]:
+		s = 1.0/(1 + math.exp(-float(a)*1.0))
+		sigmoid.append(s)
+	data.append(sigmoid)
+	return data
+#将处理后的数据写回文件
+def write_sig(file,data):
+	dataframe = pd.DataFrame({'Source':data[0],'Target':data[1],'Sigmoid':data[2]})
+	dataframe.to_csv(file,index=False,sep=',')
+#计算α	
 def get_Alpha():
-	Alpha = 0.3
-	return Alpha
-	
-def combine_files(file1,file2):
-	alpha = get_Alpha()
-	data1 = data_process_2(file1)
-	data2 = data_process_2(file2)
-	new_source,new_target,new_weight = [],[],[]
+	α = 0.3
+	return α
+#使用已有的文件按要求生成新文件	
+def combine_files(file1,file2,x):
+	α = get_Alpha()
+	data1 = data_process(file1)
+	data2 = data_process(file2)
+	new_source,new_target,new_sigmoid = [],[],[]
 	data = []
-	i=0
-	for (s1,t1,w1,si1) in zip(data1[0],data1[1],data1[2],data1[3]):
-		j=0
+	for (s1,t1,si1) in zip(data1[0],data1[1],data1[2]):
+		j = 0
 		flag = 0
-		for (s2,t2,w2,si2) in zip(data2[0],data2[1],data2[2],data2[3]):
-			if s1==s2 and t1==t2:
+		for (s2,t2,si2) in zip(data2[0],data2[1],data2[2]):
+			if s1 == s2 and t1 == t2:
 				flag = 1
 				new_s = s1
 				new_t = t1
-				new_w = int(alpha * w1) + int((1-alpha) * w2)
-				del data2[0][j]
-				del data2[1][j]
-				del data2[2][j]
-				del data2[3][j]
+				new_si = (α * float(si1)) + ((1-α) * float(si2))
+				for i in range(0,3):	#删除已计算的数据项
+					del data2[i][j]
 			j = j + 1
 		if flag == 0:
 			new_s = s1
 			new_t = t1
-			new_w = int(alpha * w1)
+			new_si = (α * float(si1))
 		new_source.append(new_s)
 		new_target.append(new_t)
-		new_weight.append(new_w)
-	for (s2,t2,w2,si2) in zip(data2[0],data2[1],data2[2],data2[3]):
+		new_sigmoid.append(new_si)
+	for (s2,t2,si2) in zip(data2[0],data2[1],data2[2]):
 		new_s = s2
 		new_t = t2
-		new_w = int((1-alpha) * w2)
+		new_si = ((1-α) * float(si2))
 		new_source.append(new_s)
 		new_target.append(new_t)
-		new_weight.append(new_w)
+		new_sigmoid.append(new_si)
 	data.append(new_source)
 	data.append(new_target)
-	data.append(new_weight)
-	file = '1.csv'
-	data = Sigmoid(data)
+	data.append(new_sigmoid)
+	s=str(x)
+	file = s+'.csv'
 	write_sig(file,data)	
-	return file		
+	return file	
+		
+def main():	
+	file = []
+	file.append('edge2015_1.csv')
+	file.append('edge2015_2.csv')
+	file.append('edge2016_1.csv')
+	file.append('edge2016_2.csv')
+	file.append('edge2017_1.csv')
+	file.append('edge2017_2.csv')
+	file.append('edge2018_1.csv')
+	for i in range(0,7):			#依次处理所有文件的数据			
+		data = Sigmoid(data_process(file[i]))
+		del data[2]
+		write_sig(file[i],data)
+	x=1
+	for i in range(0,12,2):
+		f=combine_files(file[i],file[i+1],x)
+		file.insert(i+2,f)
+		x=x+1
 	
-file = []
-file.append('edge2015_1.csv')
-file.append('edge2015_2.csv')
-file.append('edge2016_1.csv')
-file.append('edge2016_2.csv')
-file.append('edge2017_1.csv')
-file.append('edge2017_2.csv')
-file.append('edge2018_1.csv')
-for i in range(0,7):
-	data1 = data_process(file[i])	
-	#print(weight1)
-	data1 = Sigmoid(data1)
-	print(data1[3])
-	write_sig(file[i],data1)
-
-combine_files(file[0],file[1])
+main()
